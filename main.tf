eef9fae92ab0f45543270306798674217bc03925
Moosa Khalid <moosa.khalid@linuxacademy.com>
2021-01-19 16:50:51 -0500

    add tf code

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ssm_parameter" "ami_id" {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
+resource "aws_instance" "example" {
+  ami           = data.aws_ssm_parameter.ami_id.value
+  instance_type = "t3.micro"
+}
+
