98c4b6608fb42ab034acfa522d34e201f6183d51
Moosa Khalid <moosa.khalid@linuxacademy.com>
2021-01-20 02:06:50 -0500

    new policies

--- main.tf
+++ main.tf
@@ -5,8 +5,39 @@ provider "aws" {
 data "aws_ssm_parameter" "ami_id" {
   name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
+
+resource "aws_vpc" "my-vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
 resource "aws_instance" "example" {
-  ami           = data.aws_ssm_parameter.ami_id.value
-  instance_type = "t2.micro"
+  ami             = data.aws_ssm_parameter.ami_id.value
+  instance_type   = "t3.micro"
+  security_groups = [aws_security_group.my-sg.id]
+}
+
+resource "aws_subnet" "my-subnet" {
+  vpc_id     = aws_vpc.my-vpc.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_security_group" "my-sg" {
+  name   = "aws-sg"
+  vpc_id = aws_vpc.my-vpc.id
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "tf-cloud-generated-sg"
+  }
 }
 
